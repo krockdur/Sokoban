2fe21e1c1b9ff5337f0df89a53d9c23a4565214f
Julien Fougery <web@fougery.ovh>
2023-04-03 08:14:33 +0200

    Clean Project folder

--- src_old/src/Config.cpp
+++
@@ -1,8 +0,0 @@
-#include "Config.h"
-
-
-char Config::c_wall_tile = '#';
-char Config::c_objective_tile = 'C';
-char Config::c_empty_tile = ' ';
-char Config::c_pillar_tile = '$';
-char Config::c_box_tile = 'B';
