961331b3e6a6ec8f2bd383cf929ae2e262b64f65
krockdur <web@fougery.ovh>
2024-10-22 05:45:41 +0200

    t

--- src_sdl/snake.c
+++ src_sdl/snake.c
@@ -1,7 +1,7 @@
 #include "snake.h"
 
 int tab_snake[SNAKE_LENGTH][2] = { 0 };
-
+int snake_length = 4;
 
 void init_snake()
 {
@@ -12,4 +12,36 @@ void set_snake_head_position( int x, int y )
 {
     tab_snake[0][0] = x;
     tab_snake[0][1] = y;
+}
+
+void set_snake_element_position( int e, int x, int y )
+{
+    tab_snake[e][0] = x;
+    tab_snake[e][1] = y;
+}
+
+void move_snake_left()
+{
+    int new_tab_snake[SNAKE_LENGTH][2] = { 0 };
+
+    for (int i = 0; i < snake_length; i++)
+    {
+        new_tab_snake[i][0] = tab_snake[i+1][0];
+        new_tab_snake[i][1] = tab_snake[i+1][1];
+    }
+}
+
+void move_sanke_right()
+{
+
+}
+
+void move_sanke_top()
+{
+
+}
+
+void move_sanke_down()
+{
+
 }
--- src_sdl/snake.h
+++ src_sdl/snake.h
@@ -3,9 +3,18 @@
 
 #define SNAKE_LENGTH 150
 
+// variables
 extern int tab_snake[SNAKE_LENGTH][2];
+extern int snake_length;
 
+
+// fonctions
 extern void init_snake();
 extern void set_snake_head_position( int, int);
+extern void set_snake_element_position( int, int, int);
+extern void move_snake_left();
+extern void move_sanke_right();
+extern void move_sanke_top();
+extern void move_sanke_down();
 
 #endif
